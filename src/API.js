6373edec5724b9852ab38b75c653028fe61bda91
shoperola <131758988+shoperola@users.noreply.github.com>
2023-04-25 15:09:05 +0530

    Update API.js

--- src/API.js
+++ src/API.js
@@ -1,2 +1,2 @@
-export const API = "https://shoperola.herokuapp.com";
+export const API = "https://adventurous-seal-coat.cyclic.app";
 export const orderAPI = "https://shoperola.herokuapp.com/api/transaction/statuses"
